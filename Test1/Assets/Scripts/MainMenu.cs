d861321c9cfd8621f440052f9d1da1f8850302e0
Tyler <tyleradkins147@gmail.com>
2018-04-25 12:06:17 -0500

    asdf

--- Test1/Assets/Scripts/MainMenu.cs
+++ Test1/Assets/Scripts/MainMenu.cs
@@ -7,17 +7,22 @@ public class MainMenu : MonoBehaviour {
 
 	public void PlayGame()
     {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        //TODO: Make this work.
+
         //If there is not a save file, go to setup menu. If there is, go to game screen
-        if(!PlayerPrefs.HasKey("save"))
-        {
-            this.gameObject.SetActive(false);
-            var setup = new SetupMenu();
-            setup.gameObject.SetActive(true);
-        }
-        else
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-        
+        //if(!PlayerPrefs.HasKey("save"))
+        //{
+        //    this.gameObject.SetActive(false);
+
+        //    //setup.gameObject.SetActive(true);
+        //}
+        //else
+        //{
+        //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //}
+
     }
 }
