58a9e4f8f1f273b0faf8e1474a3f47fa0ba91181
meghantennant <meghantennant@utexas.edu>
2018-04-25 23:06:21 -0500

    Menu updates, added Project name to menus

--- Test1/Assets/Scripts/SetupMenu.cs
+++ Test1/Assets/Scripts/SetupMenu.cs
@@ -2,13 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using VoxelBusters.NativePlugins;
 
 public class SetupMenu : MonoBehaviour {
 
-    //Function saving university name can be found in the SaveManager class
+    public Text Input;
+    public string uniName;
+    public GameObject MainMenu;
+    public SaveManager SaveManager;
 
+    public void Awake()
+    {
+        SaveManager = MainMenu.GetComponent<MainMenu>().SaveManager;
+    }
     public void OnClickOkButton()
     {
+        SaveManager.SetUniversityName(uniName);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        NPBinding.UI.ShowToast("Welcome to " + uniName + "!", eToastMessageLength.SHORT);
+    }
+
+    public void EditUniName(string inputName)
+    {
+        uniName = inputName;      
     }
 }
